9f96cdf13913e43d2d610de8357edefc443f0f63
ZHU, ZICHEN <11926216+frederick-zhu@users.noreply.github.com>
2018-04-18 16:00:58 -0400

    提供了VR的测试接口

--- prediction/prediction_server/models.py
+++ prediction/prediction_server/models.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 import arrow
 from werkzeug.datastructures import ImmutableMultiDict
@@ -7,7 +7,8 @@ from werkzeug.datastructures import ImmutableMultiDict
 def checkParameters(
         args: ImmutableMultiDict,
         parametersList: List[str],
-        parameterOptions=None):
+        parameterType: Dict[str, List[type]] = None,
+        parameterOptions: Dict[str, List[str]] = None):
     if parameterOptions is None:
         parameterOptions = {}
 
@@ -23,6 +24,22 @@ def checkParameters(
                 'errorCode': 100,
                 'errorInfo': 'Missing parameters',
                 'missingParameters': missing_parameter
+            }
+        }
+
+    invalid_type_parameters = []
+    for k, v in parameterType.items():
+        if type(args.get(k)) not in v:
+            invalid_type_parameters.append(k)
+    if invalid_type_parameters:
+        return {
+            'type': 'error',
+            'time': arrow.utcnow().isoformat(),
+            'error': {
+                'errorCode': 102,
+                'errorInfo': 'Invalid parameter type',
+                'invalidParametersType': [{'parameter': ele, 'types': [e.__name__ for e in parameterType[ele]]} for ele
+                                          in invalid_type_parameters]
             }
         }
 
--- prediction/prediction_server/views.py
+++ prediction/prediction_server/views.py
@@ -1,3 +1,5 @@
+import random
+
 import arrow
 from flask import request, jsonify
 
@@ -11,14 +13,63 @@ def index_page():
     return 'hello, world!'
 
 
+@app.route('/api/v0.1.0/vr')
+@jsonp
+def indicator():
+    check_result = checkParameters(
+        args=request.args,
+        parametersList=['timestamp', 'symbol'],
+        parameterType={
+            'timestamp': [str, ],
+            'symbol': [str, ]
+        }
+    )
+    if check_result:
+        return jsonify(check_result)
+
+    # timestamp = arrow.get(request.args.get('timestamp', 0))
+    # data_list = []
+
+    # TODO
+
+    try:
+        timestamp = arrow.get(request.args.get('timestamp', 0))
+    except:
+        return jsonify({
+            'type': 'error',
+            'time': arrow.utcnow().isoformat(),
+            'error': {
+                'errorCode': 103,
+                'errorInfo': 'please use ISO8601 format'
+            }
+        })
+
+    res = {
+        'type': 'result',
+        'time': arrow.utcnow().isoformat(),
+        'result': {
+            'symbol': request.args.get('symbol', 'ERROR'),
+            'indicator': 'VR',
+            'timestamp': timestamp.isoformat(),
+            'data': int(random.random() * 100000) / 100
+        }
+    }
+
+    return jsonify(res)
+
+
 @app.route('/api/v0.1.0/predict')
 @jsonp
 def predict():
     # check parameters
     check_result = checkParameters(
-        request.args,
-        ['symbol', 'term'],
-        {
+        args=request.args,
+        parametersList=['symbol', 'term'],
+        parameterType={
+            'symbol': [str, ],
+            'term': [str, ]
+        },
+        parameterOptions={
             'term': ['short', 'long']
         })
 
@@ -30,7 +81,7 @@ def predict():
         'time': arrow.utcnow().isoformat(),
         'result': {
             'symbol': request.args.get('symbol'),
-            'predictPrice': 123.45,
+            'predictPrice': int(random.random() * 100000) / 100,
             'note': 'ONLY OFR TESTING!'
         }
     }
