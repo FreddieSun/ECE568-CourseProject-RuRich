4938110a92aa2d124ec75ad67f2e33583593c426
ZHU, ZICHEN <11926216+frederick-zhu@users.noreply.github.com>
2018-04-18 15:03:53 -0400

    加入了SVR

--- prediction/prediction_engine/svr.py
+++ prediction/prediction_engine/svr.py
@@ -2,28 +2,30 @@ from os import cpu_count
 
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import BayesianRidge
 from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import make_pipeline, Pipeline
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 
 from prediction_engine.get_data import get_long_term_data
 
 
-class Bayes(object):
+class SupportVectorRegression(object):
 
     @staticmethod
     def predict(X: np.ndarray, y: np.ndarray, x: np.ndarray):
         pipe = make_pipeline(
             StandardScaler(),
-            PolynomialFeatures(7),
-            BayesianRidge(normalize=False)
-        )  # type: Pipeline
+            SVR(kernel='rbf', C=1e3, gamma=10)
+        )
 
         grid_search = GridSearchCV(
             pipe,
             param_grid={
-                'polynomialfeatures__degree': list(range(40))
+                # 'svr__kernel': ['rbf', 'poly'],
+                # 'svr__degree': list(range(10)),
+                'svr__gamma': [0.1, 0.3, 1, 3, 10, 30],
+                'svr__C': [1e3, ]
             },
             n_jobs=cpu_count(),
             verbose=3
@@ -38,8 +40,6 @@ if __name__ == '__main__':
     x, y = get_long_term_data('GOOG')
 
     plt.plot(x, y, 'r-')
-    plt.plot(x, Bayes.predict(x, y, x), 'b-')
+    plt.plot(x, SupportVectorRegression.predict(x, y, x), 'b-')
 
     plt.show()
-
-    # print(Bayes.predict(x, y, x))
